e4d26d9f9d2429856a1639513384069ca8457266
Juan Wu <juanwu8238@gmail.com>
2024-05-19 17:42:23 -0400

    Solo Ajuste de diseño y animacion, LISTO

--- Practica_de_clase/Practicar_para_flet/In_animacion.py
+++ Practica_de_clase/Practicar_para_flet/In_animacion.py
@@ -2,8 +2,8 @@ import flet as ft
 
 def main (page:ft.Page):
     page.title = 'Sencillo_practica'
-    page.window_width = 750
-    page.window_height = 750
+    page.window_width = 350
+    page.window_height = 200
     page.window_resizable = False
     page.window_maximizable = False
     page.window_center()
@@ -14,15 +14,18 @@ def main (page:ft.Page):
     tf2 = ft.Text("Biennnn!",theme_style=ft.TextThemeStyle.DISPLAY_LARGE)
 
     #Contenedor
-    cont1 = ft.Container(ft.Row([tf1]))
-    cont2 = ft.Container(ft.Row([tf2]))
+    cont1 = ft.Container(ft.Row([tf1]),width=250,height=75,bgcolor="#fbf994")
+    cont2 = ft.Container(ft.Row([tf2]),width=250,height=75,bgcolor="#cdfde6")
 
     #Boton de cambiar y la muestra de cambio
-    animate = ft.AnimatedSwitcher(cont1,
-        transition=ft.AnimatedSwitcherTransition.ROTATION,
+    animate = ft.AnimatedSwitcher(
+        cont1,
+        transition=ft.AnimatedSwitcherTransition.ROTATION, #Aqui le puede cambiar varios formas para la animacion, por ejemplo, scale o rotation
         duration = 500,
         reverse_duration= 100,
-        switch_in_curve=ft.AnimationCurve.BOUNCE_OUT
+        #switch_in_curve=ft.AnimationCurve.BOUNCE_OUT este rota mas rapido
+        switch_in_curve=ft.AnimationCurve.BOUNCE_OUT,
+        switch_out_curve=ft.AnimationCurve.BOUNCE_IN,
     )
 
     def donghua (e):
@@ -30,8 +33,11 @@ def main (page:ft.Page):
         animate.update()
     
     tf = ft.TextButton("¡SI!",on_click=donghua)
-
-    contf = ft.Container(ft.Row([animate,tf],alignment=ft.MainAxisAlignment.CENTER))
+    contf = ft.Container(ft.Column([
+        ft.Row([animate],alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([tf],alignment=ft.MainAxisAlignment.CENTER),
+        ],
+        spacing=35))
 
     page.add(contf)
 ft.app(main)
