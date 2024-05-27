9504d4b38054fd032e5a0fb129a5e5c09cbc2911
Juan Wu <juanwu8238@gmail.com>
2024-05-27 15:19:18 -0400

    Un sencillo ejemplo de Matplotlib

---
+++ Practica_de_clase/Clase_6_Matplotlib/Sencillo_ex.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as sb
+
+x = [1,2,3,4,5,6,7,8]
+
+y = [2,4,6,8,10,12,14,16]
+
+sb.plot(x,y)
+
+sb.xlabel("Titulo-x")
+sb.ylabel("Titulo-y")
+sb.title("Linea de grafica")
+
+sb.show
