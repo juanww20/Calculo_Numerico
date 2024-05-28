5d729e510cedc215b2d70b25651d5d548c5aa981
Juan Wu <juanwu8238@gmail.com>
2024-05-28 10:53:23 -0400

    Agregar el algoritmo para invertir un numero entero

--- Practica_de_clase/Clase_6_Practica/Investigativa.py
+++ Practica_de_clase/Clase_6_Practica/Investigativa.py
@@ -1,5 +1,6 @@
 #Aqui presenta algunas practicas y investigativas
 
+#Potencia
 def potencia (n):
     if n == 0:
         return 1
@@ -9,3 +10,12 @@ def potencia (n):
 x = 5
 result = potencia(x)
 print(result)
+
+#Inverso
+num = 325
+invertido = 0
+while num != 0:   
+    ultimo = num%10
+    invertido = invertido*10 + ultimo
+    num = num // 10
+print(invertido)
