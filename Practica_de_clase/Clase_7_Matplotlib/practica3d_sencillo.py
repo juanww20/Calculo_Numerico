3488de9305e369ecc2b393112f1d3fdda715fca3
Juan Wu <juanwu8238@gmail.com>
2024-06-01 21:39:14 -0400

    Completacion los restos vectores y ya muestra la grafica final

--- Practica_de_clase/Clase_7_Matplotlib/practica3d_sencillo.py
+++ Practica_de_clase/Clase_7_Matplotlib/practica3d_sencillo.py
@@ -24,8 +24,34 @@ grafica = fig.add_subplot(111, projection = '3d')
 #Punto en el espacio
 [x,y,z] = x0
 grafica.scatter(x,y,z, c='blue', marker = 'o', label = 'x0')
+[x,y,z] = x1
+grafica.scatter(x,y,z, c='red', marker = 'o', label = 'x1')
+[x,y,z] = x2
+grafica.scatter(x,y,z, c='green', marker = 'o', label = 'x2')
+
+linea = np.concatenate(([x0],[x1]), axis=0)
+x = linea[:,0]
+y = linea[:,1]
+z = linea[:,2]
+grafica.plot(x,y,z, label='||x1||')
+
+linea = np.concatenate(([x0],[x2]), axis=0)
+x = linea[:,0]
+y = linea[:,1]
+z = linea[:,2]
+grafica.plot(x,y,z, label='||x2||')
+
+linea = np.concatenate(([x1],[x2]), axis=0)
+x = linea[:,0]
+y = linea[:,1]
+z = linea[:,2]
+grafica.plot(x,y,z, label='||error||')
+
 grafica.set_xlabel('eje x')
 grafica.set_ylabel('eje y')
 grafica.set_zlabel('eje z')
+grafica.legend()
+
+grafica.view_init(35,25) #La posicion de Observacion para ver la grafica
 
 plt.show()
