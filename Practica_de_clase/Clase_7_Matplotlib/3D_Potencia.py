1e2dc5d5163c5fb727998e330a49f7e7745925ec
Juan Wu <juanwu8238@gmail.com>
2024-06-02 20:47:32 -0400

    Agregar la visualizacion para la grafica de dicha matriz

--- Practica_de_clase/Clase_7_Matplotlib/3D_Potencia.py
+++ Practica_de_clase/Clase_7_Matplotlib/3D_Potencia.py
@@ -1,5 +1,6 @@
 #Importacion de la libreria
 import numpy as np
+from plotly import graph_objects as go
 
 #Implemnetacion de Metodo de Potencia
 def metodo_potencia(A, x0, tol=1e-6, max_iter=100):
@@ -21,3 +22,24 @@ x0 = np.array([1, 1])
 
 autovalor, autovector = metodo_potencia(a, x0)
 #En este caso, el autovalor es el dominante para esta matriz dicha y se muestra el autovector correspondiente
+
+# Visualización 3D del autovector
+fig = go.Figure()
+fig.add_trace(go.Scatter3d(
+    x = [0, autovector[0]],
+    y = [0, autovector[1]],
+    z = [0, 0],
+    mode = 'lines',
+    line = dict(color='blue', width=5),
+    name = 'El autovector dominante'
+))
+fig.update_layout(
+    title = 'Visualización para el dominante vector de la matriz "A"',
+    scene = dict(
+        xaxis = dict(title='X'),
+        yaxis = dict(title='Y'),
+        zaxis = dict(title='Z')
+    )
+)
+
+fig.show()
